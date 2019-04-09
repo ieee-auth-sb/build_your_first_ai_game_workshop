4c8e3d0e97ad250b6b34d84af81b9d614cb355e1
Nikolas Malamas <malamasn@hotmail.com>
2019-04-09 19:38:34 +0300

    Update ai_game using keras

--- ai_game.py
+++ ai_game.py
@@ -1,76 +1,73 @@
-import retro, cv2, gym
+import gym
 import numpy as np
+import sys
+from collections import deque
+# sys.path.append('/usr/local/lib/python2.7/site-packages')
+import cv2
+
+from keras.models import load_model, Sequential
+from keras.layers.convolutional import Convolution2D
+from keras.optimizers import Adam
+from keras.layers.core import Activation, Dropout, Flatten, Dense
+
+# Preprocess
+# # DOWNLOAD CV2
+# def preprocess(observation):
+#     observation = cv2.cvtColor(cv2.resize(observation, (84, 110)), cv2.COLOR_BGR2GRAY)
+#     observation = observation[26:110,:]
+#     ret, observation = cv2.threshold(observation,1,255,cv2.THRESH_BINARY)
+#     return np.reshape(observation,(84,84,1))
+
+
 
 # Load game's environment
-## DOWNLOAD MORE GAMES
-def init():
-    env = gym.make('SpaceInvaders-v0')
-    # Check game's specs
-    actions = env.action_space.n
-    print(actions)
-    print(env.observation_space.shape)
-    # Q-Learning init
-    Q = np.zeros([84, 84, actions])
-    eta = .628
-    gma = .9
-    epis = 5000
-    rev_list = [] # rewards per episode calculate
-# Preprocess
-# DOWNLOAD CV2
-def preprocess(observation):
-    observation = cv2.cvtColor(cv2.resize(observation, (84, 110)), cv2.COLOR_BGR2GRAY)
-    observation = observation[26:110,:]
-    ret, observation = cv2.threshold(observation,1,255,cv2.THRESH_BINARY)
-    return np.reshape(observation,(84,84,1))
-
-def init_network():
-    pass
-
-def train(epochs = 1000):
-    init_network()
-    for i in range(epochs):
-        s = env.reset()
-        # Reshape state
-        s = preprocess(s)
-        rAll = 0
-        d = False
-        j = 0
-        while j < 99:
-            env.render()
-            j += 1
-            a = np.argmax(Q[s,:] + np.random.randn(1,action_space)*(1./(i+1)))
-            #Get new state & reward from environment
-            s1,r,d,_ = env.step(a)
-            s1 = preprocess(s1)
-            #Update Q-Table with new knowledge
-            Q[s,a] = Q[s,a] + eta*(r + gma*np.max(Q[s1,:]) - Q[s,a])
-            rAll += r
-            s = s1
-            if d == True:
-                break
-        rev_list.append(rAll)
-        env.render()
-    print "Reward Sum on all episodes " + str(sum(rev_list)/epis)
-    print "Final Values Q-Table"
-    print Q
+env = gym.make('SpaceInvaders-v0')
+# Check game's specs
+actions = env.action_space.n
+# print(actions)
+# print(env.observation_space.shape)
 
 
-        # while True:
-        #     # Keras Learning
-        #
-        #     # Next is BrainDQL case
-        #     action = brain.getAction()
-        #     actionmax = np.argmax(np.array(action))
-        #
-        #     nextObservation,reward, done, info = env.step(actionmax)
-        #
-        #     if done:
-        #         nextObservation = env.reset()
-        #     nextObservation = preprocess(nextObservation)
-        #     brain.setPerception(nextObservation,action,reward,terminal)
+# Q-Learning init
+# Q = np.zeros([84, 84, actions])
+# eta = .628
+epsilon = 1.
+gamma = .9
+epochs = 5000
+memory = deque()
 
-    env.close()
+# Initialize neural network
+model = Sequential()
+model.add(Dense(128, input_shape=(84,84), activation='relu'))
+# model.add(Dense(128, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(actions, activation='linear'))
+model.compile(loss='mse', optimizer=Adam())
+print("Successfully constructed networks.")
 
-if __name__ == '__main__':
-    init()
-    train()
+# Predict action
+def act(state):
+    if np.random.rand() <= epsilon:
+        return random.randrange(actions)
+    act_values = model.predict(state)
+    return np.argmax(act_values[0])  # returns action
+
+
+# Train model
+for e in range(10):
+
+    state = env.reset()
+    state = preprocess(state)
+
+    for play in range(100):
+        # Get action
+        action = act(state)
+        next_state, reward, done, _ =env.step(action)
+        next_state = preprocess(next_state)
+
+        memory.append((state, action, reward, next_state, done))
+
+        state = next_state
+        if done:
+            print('Done at play: ', play)
+            break
